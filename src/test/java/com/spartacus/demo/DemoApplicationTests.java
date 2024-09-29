1af815a587773ba8fa2bda1e937d856d6a3a9e05
Carl Thronson <carlthronson@gmail.com>
2024-09-29 15:38:11 -0700

    Adding a default app created by spring initializr

---
+++ src/test/java/com/spartacus/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spartacus.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
